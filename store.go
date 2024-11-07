3ffc8366128d7e2334648aa03d6a0ab0e33e84b8
Rafey Rana <24100103@lums.edu.pk>
2024-11-06 23:32:33 -0500

    folders being created from hashing

--- store.go
+++ store.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"crypto/sha1"
+	"crypto/md5"
+	"encoding/hex"
 	"io"
-	"os"
 	"log"
+	"strings"
+	"os"
+	"bytes"
 )
+
+
+func CASPathTransformFunc(key string) string{
+	// here we will be implementing our key hashing
+	// decisions to be made here : SHA1, MD5 or SHA256 
+	// we are using SHA1
+	hash  := sha1.Sum([]byte(key))
+	hashString := hex.EncodeToString(hash[:]) // trick to convert to slice [:]
+	blockSize := 5 // depth for block
+	sliceLength := len(hashString) / blockSize
+
+	paths := make([]string, sliceLength)
+	for i := 0; i < sliceLength; i++ {
+		from, to := i*blockSize, (i+1)*blockSize
+        paths[i] = hashString[from:to]
+    }
+	return strings.Join(paths, "/")
+}
 
 type PathTransformFunc func(key string) string
 
@@ -12,8 +35,6 @@ type StoreOpts struct {
 
 	PathTransformFunc PathTransformFunc
 }
-
-
 
 
 var DefaultPathTransformFunc = func(key string) string {
@@ -37,7 +58,13 @@ func (s *Store) writeStream(key string, r io.Reader)  error {
 		return err
 		}
 
-	filename := "someFilename"
+
+
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
+
+	filenameBytes := md5.Sum(buf.Bytes())
+	filename := hex.EncodeToString(filenameBytes[:])
 
 	pathAndFilename := pathName + "/" + filename
 	f, err := os.Create(pathAndFilename)
@@ -45,7 +72,7 @@ func (s *Store) writeStream(key string, r io.Reader)  error {
         return err
     }
 
-	n , err := io.Copy(f, r)
+	n , err := io.Copy(f, buf)
 	if err!= nil {
         return err
     }
--- store_test.go
+++ store_test.go
@@ -2,17 +2,29 @@ package main
 import (
 	"testing"
     "bytes"
+
 )
 
 
+
+func TestPathTransformFuncs(t *testing.T) {
+	key := "momsbestpicture"
+	pathName := CASPathTransformFunc(key)
+	expectedPath := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathName != expectedPath {
+		t.Errorf("expected %s, got %s", expectedPath, pathName)
+	}
+	
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc : DefaultPathTransformFunc,
+		PathTransformFunc : CASPathTransformFunc,
 	}
 	s := NewStore(opts)
 
 	data := bytes.NewReader([]byte("some jpeg bytes"))
-	err := s.writeStream("mySpecialKey", data)
+	err := s.writeStream("momsbestpicture", data)
 	if err != nil {
 		t.Error(err)
 	}
