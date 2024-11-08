2a417cdae1471da521a4ac4c48ef095c0c0a3af2
Rafey Rana <24100103@lums.edu.pk>
2024-11-08 11:38:06 -0500

    read file using key implemented

--- store.go
+++ store.go
@@ -8,11 +8,12 @@ import (
 	"log"
 	"os"
 	"strings"
+	"bytes"
 )
 
 
 func CASPathTransformFunc(key string) PathKey{
-	// here we will be implementing our key hashing
+	// here we will be implementing our key ing
 	// decisions to be made here : SHA1, MD5 or SHA256 
 	// we are using SHA1
 	hash  := sha1.Sum([]byte(key))
@@ -28,21 +29,21 @@ func CASPathTransformFunc(key string) PathKey{
 
 	return PathKey{
 		PathName:  strings.Join(paths, "/"),
-        Original:  hashString,
+        FileName:  hashString,
 	}
 	
 }
 type PathKey struct {
 	PathName string
-	Original string
+	FileName string
 
 }
 
 type PathTransformFunc func(key string) PathKey
 
 
-func(p PathKey) filename() string { 
-	return	fmt.Sprintf("%s/%s",p.PathName, p.Original)
+func(p PathKey) FullPath() string { 
+	return	fmt.Sprintf("%s/%s",p.PathName, p.FileName)
 }
 
 type StoreOpts struct {
@@ -65,6 +66,28 @@ func NewStore(storeOpts StoreOpts) *Store{
 	}
 }
 
+
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+	f.Close()
+	return buf, nil
+}
+
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	pathAndFilename := pathKey.FullPath()
+	return os.Open(pathAndFilename)
+	
+}
+
+
 func (s *Store) writeStream(key string, r io.Reader)  error {
 	pathKey := s.PathTransformFunc(key)
 
@@ -74,7 +97,7 @@ func (s *Store) writeStream(key string, r io.Reader)  error {
 
 
 
-	pathAndFilename := pathKey.filename()
+	pathAndFilename := pathKey.FullPath()
 	f, err := os.Create(pathAndFilename)
 	if err!= nil {
         return err
--- store_test.go
+++ store_test.go
@@ -1,34 +1,44 @@
 package main
+
 import (
+	"bytes"
+	"io/ioutil"	
+	"fmt"
 	"testing"
-    "bytes"
-
 )
-
-
 
 func TestPathTransformFuncs(t *testing.T) {
 	key := "momsbestpicture"
-	
+
 	pathKey := CASPathTransformFunc(key)
 	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
 	expectedPath := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
-	if pathKey.Original != expectedOriginalKey || pathKey.PathName != expectedPath {
+	if pathKey.FileName != expectedOriginalKey || pathKey.PathName != expectedPath {
 		t.Errorf("expected %s, got %s", expectedPath, pathKey.PathName)
 	}
-	
+
 }
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc : CASPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
-
-	data := bytes.NewReader([]byte("some jpeg bytes"))
-	err := s.writeStream("myspecialpictures", data)
+	key := "myspecialpictures"
+	data := []byte("some jpeg bytes")
+	err := s.writeStream(key, bytes.NewReader(data))
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := s.Read(key)
 	if err != nil {
 		t.Error(err)
 	}
 
+	b, _ := ioutil.ReadAll(r)
+	fmt.Println("this is the data:", string(b))
+	if string(b) != string(data) {
+		t.Errorf("expected data : %s, got data from reading: %s", data, b)
+	}
+	
 }
